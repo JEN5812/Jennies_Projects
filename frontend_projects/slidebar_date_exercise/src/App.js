fcfa7fc94dd9b00746459ad19e5160d41f2d5009
Jennie Butch <jenniebutch@Jennies-MacBook-Pro.local>
2025-03-21 13:15:44 -0700

    Slidebar date exercise

---
+++ frontend_projects/slidebar_date_exercise/src/App.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  function handleReset(c) {
+    // c.preventDefault();
+
+    // if (!count) return;
+
+    setCount(0);
+    setStep(1);
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          value={step}
+          min="0"
+          max="10"
+          // we need number in order to count correctly and not just add on like 010
+          onChange={(c) => setStep(Number(c.target.value))}
+        />
+
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(c) => setCount(Number(c.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
